293553ba3d097c6857ce1e97184e0e4820290602
cappochinho <edwardprempeh18@gmail.com>
2023-05-06 17:58:37 +0100

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,14 +3,14 @@
 const id = process.argv[2];
 const endpoint = 'https://swapi-api.alx-tools.com/api/films/' + id;
 const request = require('request');
-request(endpoint, { json: true } ,function (error, response, body) {
+request(endpoint, { json: true }, function (error, response, body) {
   if (error) throw error;
   const res = body.characters;
-  res.forEach((character) => {
+  for (const character of res) {
     request(character, { json: true }, function (error, response, body) {
       if (error) throw error;
       const res2 = body.name;
       console.log(res2);
     });
-  });
+  }
 });
