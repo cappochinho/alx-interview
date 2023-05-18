2fb72df31d922d00fc1f9980b9780772bb1f30ca
cappochinho <edwardprempeh18@gmail.com>
2023-05-18 20:15:49 +0100

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,6 +2,7 @@
 """Module for Task 0: Making change
 """
 
+
 def makeChange(coins, total):
     """Making change
     """
@@ -9,8 +10,8 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-
+    inf = float('inf')
+    dp = [inf] * (total + 1)
     dp[0] = 0
 
     for i in range(1, total + 1):
@@ -18,7 +19,4 @@ def makeChange(coins, total):
             if coin <= i:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+    return -1 if dp[total] == inf else dp[total]
