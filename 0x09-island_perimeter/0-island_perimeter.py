bbbcf2f63df9a8a4c0878e604c3bb5443cd576b9
cappochinho <edwardprempeh18@gmail.com>
2023-05-22 19:33:51 +0100

    Task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""0-island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """Island perimeter function"""
+
+    rows = len(grid)
+    if rows == 0:
+        return 0
+
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+    return perimeter
